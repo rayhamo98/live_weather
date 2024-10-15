5e77ff39695c4f41cc5169ee50f25ee856e8667c
godray2024 <rimonhamo@yahoo.com>
2024-10-15 18:45:05 -0400

    added routes and weather TS files

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,53 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
+
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const cityName = req.body.cityName;
+
+  if (!cityName) {
+    return res.status(400).json({ error: 'City name is required' });
+  }
   // TODO: GET weather data from city name
+  try {
+    const weatherData = await WeatherService.getWeatherForCity(cityName);
+  
   // TODO: save city to search history
+  await HistoryService.addCity(cityName);
+
+  return res.status(200).json(weatherData);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Failed to fetch weather data' });
+  }
+
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (_req, res) => {
+  try {
+    const searchHistory = await HistoryService.getCities();
+    return res.json(searchHistory);
+  } catch (error) {
+    console.error(error);
+    return res.json({ error: 'Failed to fetch search history' });
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedCity = await HistoryService.removeCity(id);
+    res.status(200).json({ deletedCity });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to delete city from search history' });
+  }
+});
 
 export default router;
